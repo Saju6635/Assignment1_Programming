57937ee0a6079cebcfae8dc14686653d7dcda7cd
Saju <Saju@LAPTOP-SDRM6T0Q>
2024-02-03 18:02:06 -0500

    Add project files.

---
+++ Assignment1_Programming/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    { 
+
+        Console.WriteLine("Choose a pet type \n 1.Cat \n 2.Rabbit \n 3.Dog ");
+
+        Console.Write("User Input: ");
+
+        int petChoice = Convert.ToInt32(Console.ReadLine());
+
+        string petType;
+
+        switch (petChoice)
+        {
+            case 1:
+                petType = "Cat";
+                break;
+            case 2:
+                petType = "Rabbit";
+                break;
+            case 3:
+                petType = "Dog";
+                break;
+            default:
+                petType = "Unknown pet type";
+                break;
+                
+        }
+       
+        Console.WriteLine($"You have chose a {petType}, Enter a name for your pet: ");
+        string petName = Console.ReadLine();
+
+        Console.WriteLine($"Welcome {petName}!, lets take good care of you");
+
+    }
+}
