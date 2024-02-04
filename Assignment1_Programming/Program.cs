bf04eff5f79950b99aae790f7cdb44f405cc37ea
Saju6635 <sajukraju77@gmail.com>
2024-02-03 19:40:54 -0500

    1. getting user input and welcome message is printed

--- Assignment1_Programming/Program.cs
+++ Assignment1_Programming/Program.cs
@@ -7,11 +7,23 @@ class Program
 
         Console.WriteLine("Choose a pet type \n 1.Cat \n 2.Rabbit \n 3.Dog ");
 
-        Console.Write("User Input: ");
+        
 
-        int petChoice = Convert.ToInt32(Console.ReadLine());
+        int petChoice;
 
-        string petType;
+        do
+        {
+            Console.Write("User Input: ");
+            petChoice = Convert.ToInt32(Console.ReadLine());
+
+            if (petChoice < 1 || petChoice > 3)
+            {
+                Console.WriteLine("Invalid Input, Choose a valid input");
+            }
+        }
+        while (petChoice < 1 || petChoice > 3);
+          
+        string petType = "";
 
         switch (petChoice)
         {
@@ -24,16 +36,13 @@ class Program
             case 3:
                 petType = "Dog";
                 break;
-            default:
-                petType = "Unknown pet type";
-                break;
                 
         }
        
         Console.WriteLine($"You have chose a {petType}, Enter a name for your pet: ");
         string petName = Console.ReadLine();
 
-        Console.WriteLine($"Welcome {petName}!, lets take good care of you");
+        Console.WriteLine($"Welcome {petName}!, lets take good care of him");
 
     }
 }
